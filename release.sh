e3ab539e08934f780b2b988f42095e1b124389f0
x-car <x-car@aurorax-car.com>
2025-11-03 15:55:04 +0800

    use docker buildx
    Signed-off-by: x-car <x-car@aurorax-car.com>

--- release.sh
+++ release.sh
@@ -9,11 +9,10 @@ git_ref=${1:-4f183a4}
 echo using reactor-c git commit ref ${git_ref}
 if [ -n "${1}" ]; then shift; fi
 
-docker build . \
+docker buildx build . \
     --build-arg RTI_USE_SSL=OFF \
     --build-arg RTI_GIT_REF=${git_ref} \
-    --platform=linux/amd64,linux/aarch64,linux/arm/v7,linux/riscv64 \
-    --tag=xronosinc/rti:reactor-c-${git_ref} \
+    --platform=linux/aarch64 \
+    --tag=soafee-cr-repo-registry-vpc.ap-southeast-1.cr.aliyuncs.com/soafee-w3/soafee-w3-avp:reactor-c-${git_ref} \
     --push \
-    --load \
-    $@
+    "$@"
